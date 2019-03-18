1b78de6797cacb31ee5e455e092aa0b009750120
Nicolas <nicoga97@gmail.com>
2019-03-18 13:24:08 -0500

    added components to trip view

--- src/Components/TripCard.js
+++ src/Components/TripCard.js
@@ -12,9 +12,25 @@ import Slide from '@material-ui/core/Slide';
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from '@material-ui/core/Grid';
-import MapContainer from './Maps/MapsContainer';
+import CardActions from "@material-ui/core/CardActions";
+import CardHeader from "@material-ui/core/CardHeader";
+import Avatar from "@material-ui/core/Avatar";
+import Collapse from "@material-ui/core/Collapse";
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import classNames from 'classnames';
 
-const styles   = theme => ({
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import ExpandLess from '@material-ui/icons/ExpandLess';
+
+const styles = theme => ({
     appBar: {
         position: 'relative',
     },
@@ -32,8 +48,26 @@ const styles   = theme => ({
         padding: theme.spacing.unit * 3,
     },
     unAbsolute: {
-          position: 'static',
-      }
+        position: 'static',
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    actions: {
+        display: 'flex',
+    },
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+    nested: {
+        paddingLeft: theme.spacing.unit * 4,
+    },
+
+
+
 
 });
 
@@ -44,6 +78,7 @@ function Transition(props) {
 class TripCard extends React.Component {
     state = {
         open: false,
+        expanded: false,
     };
 
     handleClickOpen = () => {
@@ -53,15 +88,19 @@ class TripCard extends React.Component {
     handleClose = () => {
         this.setState({open: false});
     };
+    handleExpandClick = () => {
+        this.setState(state => ({expanded: !state.expanded}));
+    };
 
     render() {
         const {classes} = this.props;
         return (
             <>
-                <Card onClick={this.handleClickOpen} >
+                <Card onClick={this.handleClickOpen}>
                     <CardContent className={classes.card}>
+
                         <Typography className={classes.title} color="textSecondary" gutterBottom>
-                            {this.props.trip.driver ?<> Eres conductor </>:<>Eres pasajero</>}
+                            {this.props.trip.driver ? <> Eres conductor </> : <>Eres pasajero</>}
                         </Typography>
                         <Typography variant="h5" component="h2">
                             Destino: {this.props.trip.destination.desc}
@@ -96,15 +135,17 @@ class TripCard extends React.Component {
                         </Toolbar>
                     </AppBar>
                     <main className={classes.content}>
-                        <Grid container   spacing={24}>
+                        <Grid container spacing={24}>
                             <Grid wrap="nowrap" item xs={12} sm={6}>
-                                <Card onClick={this.handleClickOpen} >
+                                <Card onClick={this.handleClickOpen}>
                                     <CardContent className={classes.card}>
+
                                         <Typography className={classes.title} color="textSecondary" gutterBottom>
                                             Detalles del Viaje
                                         </Typography>
                                         <Typography variant="h5" component="h2">
-                                            {this.props.trip.driver ?<> Viajas como conductor </>:<>Viajas como pasajero</>}
+                                            {this.props.trip.driver ? <> Viajas como conductor </> : <>Viajas como
+                                                pasajero</>}
                                         </Typography>
 
                                         <Typography component="p">
@@ -113,15 +154,147 @@ class TripCard extends React.Component {
                                         <Typography color="textSecondary">
                                             {this.props.trip.day}-{this.props.trip.hour}
                                         </Typography>
-                                        <div className={classes.unAbsolute}>
-                                          <MapContainer />
-                                         </div>
+                                        <br/>
+
+
+                                        {this.props.trip.driver ?
+                                            <>
+                                                <List
+                                                    component="nav"
+                                                    subheader={<ListSubheader component="div">Nested List
+                                                        Items</ListSubheader>}
+                                                    className={classes.root}
+                                                >
+                                                    <ListItem button>
+                                                        <ListItemIcon>
+
+                                                        </ListItemIcon>
+                                                        <ListItemText inset primary="Sent mail"/>
+                                                    </ListItem>
+                                                    <ListItem button>
+                                                        <ListItemIcon>
+
+                                                        </ListItemIcon>
+                                                        <ListItemText inset primary="Drafts"/>
+                                                    </ListItem>
+                                                    <ListItem button onClick={this.handleClick}>
+                                                        <ListItemIcon>
+
+                                                        </ListItemIcon>
+                                                        <ListItemText inset primary="Inbox"/>
+                                                        {this.state.open ? <ExpandLess/> : <ExpandMore/>}
+                                                    </ListItem>
+                                                    <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                                                        <List component="div" disablePadding>
+                                                            <ListItem button className={classes.nested}>
+                                                                <ListItemIcon>
+
+                                                                </ListItemIcon>
+                                                                <ListItemText inset primary="Starred"/>
+                                                            </ListItem>
+                                                        </List>
+                                                    </Collapse>
+                                                </List>
+
+                                            </>
+                                            : <>Viajas como
+                                                pasajero</>}
+
                                     </CardContent>
+                                    <CardActions>
+
+                                        <Button size="medium" color="secondary">
+                                            Cancelar Viaje
+                                        </Button>
+                                    </CardActions>
 
                                 </Card>
 
                             </Grid>
+                            <Grid wrap="nowrap" item xs={12} sm={6}>
+                                <Card className={classes.card}>
+                                    <CardHeader
+                                        avatar={
+                                            <Avatar aria-label="Recipe" className={classes.avatar}>
+                                                R
+                                            </Avatar>
+                                        }
+                                        action={
+                                            <IconButton>
+                                                <MoreVertIcon/>
+                                            </IconButton>
+                                        }
+                                        title="Shrimp and Chorizo Paella"
+                                        subheader="September 14, 2016"
+                                    />
+
+                                    <CardContent>
+                                        <Typography component="p">
+                                            This impressive paella is a perfect party dish and a fun meal to cook
+                                            together with your
+                                            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions className={classes.actions} disableActionSpacing>
+                                        <IconButton aria-label="Add to favorites">
+                                            <FavoriteIcon/>
+                                        </IconButton>
+                                        <IconButton aria-label="Share">
+                                            <ShareIcon/>
+                                        </IconButton>
+                                        <IconButton
+                                            className={classNames(classes.expand, {
+                                                [classes.expandOpen]: this.state.expanded,
+                                            })}
+                                            onClick={this.handleExpandClick}
+                                            aria-expanded={this.state.expanded}
+                                            aria-label="Show more"
+                                        >
+                                            <ExpandMoreIcon/>
+                                        </IconButton>
+                                    </CardActions>
+                                    <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                                        <CardContent>
+                                            <Typography paragraph>Method:</Typography>
+                                            <Typography paragraph>
+                                                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+                                                aside for 10
+                                                minutes.
+                                            </Typography>
+                                            <Typography paragraph>
+                                                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+                                                medium-high
+                                                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally
+                                                until lightly
+                                                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside,
+                                                leaving
+                                                chicken and chorizo in the pan. Add pimentón, bay leaves, garlic,
+                                                tomatoes, onion,
+                                                salt and pepper, and cook, stirring often until thickened and fragrant,
+                                                about 10
+                                                minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring
+                                                to a boil.
+                                            </Typography>
+                                            <Typography paragraph>
+                                                Add rice and stir very gently to distribute. Top with artichokes and
+                                                peppers, and cook
+                                                without stirring, until most of the liquid is absorbed, 15 to 18
+                                                minutes. Reduce heat
+                                                to medium-low, add reserved shrimp and mussels, tucking them down into
+                                                the rice, and
+                                                cook again without stirring, until mussels have opened and rice is just
+                                                tender, 5 to 7
+                                                minutes more. (Discard any mussels that don’t open.)
+                                            </Typography>
+                                            <Typography>
+                                                Set aside off of the heat to let rest for 10 minutes, and then serve.
+                                            </Typography>
+                                        </CardContent>
+                                    </Collapse>
+                                </Card>
+                            </Grid>
                         </Grid>
+
                     </main>
 
                 </Dialog>
